785557301e51a7dc33b28e8e0fb9db6ba7f121f3
d0422 <rlfehd2013@naver.com>
2022-05-26 23:03:15 +0900

    0526

---
+++ cointracker/src/App.js
@@ -0,0 +1,30 @@
+import {useState, useEffect} from "react";
+
+function App() {
+  const [loading, setloading]=useState(true);
+  const [coins, setcoins]=useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then((response)=> response.json())
+    .then((json)=>{
+      setcoins(json);
+      setloading(false);
+    });
+  }, []);
+  return (
+    <div>
+      <h1>Coin Tracker ({coins.length})</h1>
+      {loading ? <strong>Loading...</strong> :
+      <select>
+        {coins.map((coin) => (
+        <option>
+          {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
+          </option>))}
+        </select>
+        }
+
+    </div>
+  );
+}
+
+export default App;
